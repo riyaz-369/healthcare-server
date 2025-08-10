e4789f1ea723fc9effbd358b4ca76ff5f5d40d47
riyaz-369 <riyazulislam0099@gmail.com>
2025-08-10 15:15:09 +0600

    filtering specific filed for admins search

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -3,24 +3,44 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdminsFromDB = async (params: Record<string, unknown>) => {
+  const { searchTerm, ...restParams } = params;
+
+  const andConditions = [];
+  const adminSearchableFields = ["name", "email"];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm as string,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(restParams).length > 0) {
+    andConditions.push({
+      AND: Object.keys(restParams).map((key) => ({
+        [key]: {
+          equals: restParams[key],
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  //   console.dir(andConditions, { depth: "infinite" });
+
+  const whereConditions: Record<string, unknown> = {
+    AND: andConditions,
+  };
+
+  //   console.dir(whereConditions, { depth: "infinite" });
+
   try {
     const result = await prisma.admin.findMany({
-      where: {
-        OR: [
-          {
-            name: {
-              contains: params.searchTerm as string,
-              mode: "insensitive",
-            },
-          },
-          {
-            email: {
-              contains: params.searchTerm as string,
-              mode: "insensitive",
-            },
-          },
-        ],
-      },
+      where: whereConditions,
     });
     return result;
   } catch (error) {
