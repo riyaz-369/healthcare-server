47db0585a854fa03b956a985bc08be1636457ef2
riyaz-369 <riyazulislam0099@gmail.com>
2025-08-13 23:29:11 +0600

    generate accessToken for user login

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,5 +1,32 @@
+import prisma from "../../../utils/prisma.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const logInUser = async (payload: { email: string; password: string }) => {
-  console.log("Logging in user...", payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcdefg",
+    {
+      algorithm: "HS256",
+      expiresIn: "15m",
+    }
+  );
+
+  console.log(`Access Token: ${accessToken}`);
 };
 
 export const authService = {
