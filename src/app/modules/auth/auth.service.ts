d4d568f95f62d3235b9225778d35a531a41f4f8a
riyaz-369 <riyazulislam0099@gmail.com>
2025-08-14 00:25:53 +0600

    Generating Access Token Using Refresh Token

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,6 +2,7 @@ import generateToken from "../../../utils/generateToken.js";
 import prisma from "../../../utils/prisma.js";
 import bcrypt from "bcrypt";
 import type { LoginPayload } from "./auth.interface.js";
+import jwt from "jsonwebtoken";
 
 const logInUser = async (payload: LoginPayload) => {
   const userData = await prisma.user.findUniqueOrThrow({
@@ -35,7 +36,32 @@ const logInUser = async (payload: LoginPayload) => {
 };
 
 const refreshToken = async (token: string) => {
-  console.log("token::", token);
+  let decodedData;
+  try {
+    decodedData = jwt.verify(token, "abcdefgh");
+  } catch (error) {
+    throw new Error("You are not authorized!");
+  }
+
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: decodedData.email,
+    },
+  });
+
+  const accessToken = generateToken(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcdefg",
+    "5m"
+  );
+
+  return {
+    accessToken,
+    needPasswordChange: userData?.needPasswordChange,
+  };
 };
 
 export const authService = {
