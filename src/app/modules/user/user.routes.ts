6203da5d941c719c55ff25704007d59a439afc82
riyaz-369 <riyazulislam0099@gmail.com>
2025-08-30 09:56:52 +0600

    Uploaded Images to Current Working Directory with Multer

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { userController } from "./user.controller.js";
 import auth from "../../middlewares/auth.js";
 import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../../utils/fileUploader.js";
 
 const router = express.Router();
 
 router.post(
   "/",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single("file"),
   userController.createAdmin
 );
 
